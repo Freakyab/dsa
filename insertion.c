8bc9239ca75152837ea5170589c7b0eb68986c96
Aryan Bhisikar <aryanbhisikar1@gmail.com>
2022-12-14 07:28:45 +0000

    patch update

--- insertion.c
+++ insertion.c
@@ -4,43 +4,70 @@ int print(int arr[], int n)
     int i;
     for (i = 0; i < n; i++)
     {
-        printf("%d ", arr[i]);
+        printf("%3d ", arr[i]);
     }
     printf("\n");
 }
 
+int merge(int arr[],int n)
+{
+    int mid= n/2;
+    print(arr,n);
+    int arr1[mid],arr2[n-mid],res[5];
+    int i=0,j;
+    while(i<mid)
+    {
+        print(arr1,mid);
+        arr1[i]=arr[i];
+        print(arr1,mid);
+        i++;
+    }
+    i=0;
+    while(mid+1<n)
+    {
+         print(arr2,mid+1);
+        arr2[i]=arr[mid+1];
+         print(arr2,mid+1);
+        mid++;
+        i++;
+    }
+    print(arr1,2);
+    print(arr2,3);
+
+}
+
 int main()
 {
-    int arr[5] = {5, 4, 3, 2, 1};
-    int i, j, n = 5, temp;
+    int arr[5] = {5, 45, 3, 2, 1};
+    int i, j, n = 5, temp,min;
     print(arr, n);
-    for (i=1;i<n;i++)
-    {
-        for ( j = i+1; j<n; j++)
-        {
-            if (arr[i]<arr[j])
-            {
-                temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
-            }
-            print( arr,n);
-        }
-    }
-    // if (arr[0] > arr[1])
+    // for(i = 1; i<n;i++)
     // {
-    //     temp = arr[1];
-    //     arr[1] = arr[0];
-    //     arr[0] = temp;
+        // int temp = arr[0];
+        // for(j=i-1;j>=0;j--)
+        // {
+        //     if(arr[j]>arr[j+1])
+        //     {
+        //         print(arr,n);
+        //         temp=arr[j];
+        //         arr[j]=arr[j+1];
+        //         arr[j+1]=temp;
+        //     }
+        // }
     // }
-    // print(arr,n);
-    // if (arr[1] > arr[2])
+    // for(i=0;i<n;i++)
     // {
-    //     temp = arr[2];
-    //     arr[2] = arr[1];
-    //     arr[1] = temp;
+    //     min = i;
+    //     for(j=i+1;j<n;j++)
+    //     {
+    //         if(arr[min]>arr[j])
+    //             min = j;
+    //         temp = arr[min];
+    //         arr[min]= arr[i];
+    //         arr[i] = temp;
+    //     }
     // }
-    
+    merge(arr,n);
     // print(arr,n);
     return 0;
 }
