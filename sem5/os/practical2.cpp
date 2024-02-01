551feb4c120594b2441cbc7efa314f7b57705bb3
Freakyab <aryanbhisikar1@gmail.com>
2024-02-01 14:21:35 +0530

    Update code: Added iomanip library and removed commented code

--- sem5/os/practical2.cpp
+++ sem5/os/practical2.cpp
@@ -1,32 +1,3 @@
-// #include <iostream>
-// using namespace std;
-
-// int main()
-// {
-//     int n, time;
-//     cout << "Enter the no. of process: ";
-//     cin >> n;
-//     cout << "Enter the time slot: ";
-//     cin >> time;
-//     int b[n], rounds = 0;
-//     for (int i = 0; i < n; i++)
-//     {
-//         cout << "Enter the burst time of process " << i + 1 << ": ";
-//         cin >> b[i];
-//         rounds = b[i] > rounds ? b[i] : rounds;
-//     }
-//     for (int k = 0; k <= rounds / time; k++)
-//     {
-//         cout << "Round " << k + 1 << ": " <<endl;
-//         for (int i = 0; i <n; i++)
-//         {
-//             int timeRemaining = b[i] - (k+1)*(time) > 0 ? b[i] - (k+1)*(time) : 0;
-//             cout << "process " << i + 1 << " waiting time : " << timeRemaining   << endl;
-//         }
-        
-//     }
-// }
-
 #include <iostream>
 using namespace std;
 
